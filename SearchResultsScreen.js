4b16d80614e56b4ddb140f9b6e22a19090f209f8
Adrián Brito <adrbrpa1988@gmail.com>
2017-07-11 12:25:29 +0200

    Updated app style

--- SearchResultsScreen.js
+++ SearchResultsScreen.js
@@ -33,12 +33,29 @@ class SearchResultsScreen extends Component {
         //console.log(this.props.navigation.state.params.listings);
     }
 
+    /*
+     This method locates the property that was tapped by the user.
+     */
+    rowPressed(listerURL) {
+        var property = this.props.listings.filter(prop => prop.lister_url === listerURL)[0];
+    }
+
     renderRow(rowData, sectionID, rowID) {
+        var price = rowData.price_formatted.split(' ')[0];
+
         return (
-            <TouchableHighlight
-                underlayColor='#dddddd'>
+            <TouchableHighlight onPress={() => this.rowPressed(rowData.lister_url)}
+                                underlayColor='#dddddd'>
                 <View>
-                    <Text>{rowData.title}</Text>
+                    <View style={styles.rowContainer}>
+                        <Image style={styles.thumb} source={{ uri: rowData.img_url }} />
+                        <View  style={styles.textContainer}>
+                            <Text style={styles.price}>{price}</Text>
+                            <Text style={styles.title}
+                                  numberOfLines={1}>{rowData.title}</Text>
+                        </View>
+                    </View>
+                    <View style={styles.separator}/>
                 </View>
             </TouchableHighlight>
         );
@@ -56,5 +73,33 @@ class SearchResultsScreen extends Component {
     }
 
 }
+
+var styles = StyleSheet.create({
+    thumb: {
+        width: 80,
+        height: 80,
+        marginRight: 10
+    },
+    textContainer: {
+        flex: 1
+    },
+    separator: {
+        height: 1,
+        backgroundColor: '#dddddd'
+    },
+    price: {
+        fontSize: 25,
+        fontWeight: 'bold',
+        color: '#48BBEC'
+    },
+    title: {
+        fontSize: 20,
+        color: '#656565'
+    },
+    rowContainer: {
+        flexDirection: 'row',
+        padding: 10
+    }
+});
 
 module.exports = SearchResultsScreen;
