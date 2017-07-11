21ce17a49090f0a2f8e51da1dd0ab34661efbef0
Adrián Brito <adrbrpa1988@gmail.com>
2017-07-11 11:06:03 +0200

    Performing an API Request

--- SearchScreen.js
+++ SearchScreen.js
@@ -22,10 +22,12 @@ class SearchScreen extends Component {
      */
     constructor(props) {
         super(props);
+        // Set the initial state.
         // Component has a state variable, with searchString set to an initial value of london.
         this.state = {
             searchString: 'london',
-            isLoading: false
+            isLoading: false,
+            message: '' // Display a range of messages to the user.
         };
     }
 
@@ -38,11 +40,39 @@ class SearchScreen extends Component {
         console.log(this.state.searchString);
     }
 
+    /*
+    Will eventually run the query, but for now it simply logs a message to the console and sets isLoading
+    appropriately so the UI can show the new state.
+    */
     _executeQuery(query) {
         console.log(query);
         this.setState({ isLoading: true });
+        // This makes use of the fetch function, which is part of the Web API, and provides a vastly improved API
+        // versus XMLHttpRequest. The asynchronous response is returned as a promise, with the success path parsing
+        // the JSON and supplying it to _handleResponse.
+        fetch(query)
+            .then(response => response.json())
+            .then(json => this._handleResponse(json.response))
+            .catch(error =>
+                this.setState({
+                    isLoading: false,
+                    message: 'Something bad happened ' + error
+                }));
     }
 
+    _handleResponse(response) {
+        this.setState({ isLoading: false , message: '' });
+        if (response.application_response_code.substr(0, 1) === '1') {
+            console.log('Properties found: ' + response.listings.length);
+        } else {
+            this.setState({ message: 'Location not recognized; please try again.'});
+        }
+    }
+
+
+    /*
+     Configures and initiates the search query. This should kick off when the ‘Go’ button is pressed.
+    */
     onSearchPressed() {
         var query = urlForQueryAndPage('place_name', this.state.searchString, 1);
         this._executeQuery(query);
@@ -51,6 +81,8 @@ class SearchScreen extends Component {
     render() {
         console.log('SearchPage.render');
 
+        // Adds an activity indicator or an empty view, depending on the component’s isLoading state. Because the entire
+        // component is rendered each time, you are free to mix JSX and JavaScript logic.
         var spinner = this.state.isLoading ?
             ( <ActivityIndicator
                 size='large'/> ) :
@@ -81,12 +113,14 @@ class SearchScreen extends Component {
                 </View>
                 <Image source={require('./resources/house.png')} style={styles.image}/>
                 {spinner}
-
+                <Text style={styles.description}>{this.state.message}</Text>
             </View>
         );
     }
 }
 
+// Creates the query string based on the parameters in data. Following this, it transforms the data into the required
+// string format, name=value pairs separated by ampersands.
 function urlForQueryAndPage(key, value, pageNumber) {
     var data = {
         country: 'uk',
@@ -101,6 +135,9 @@ function urlForQueryAndPage(key, value, pageNumber) {
     var querystring = Object.keys(data)
         .map(key => key + '=' + encodeURIComponent(data[key]))
         .join('&');
+
+
+    // Example request: http://api.nestoria.co.uk/api?country=uk&pretty=1&encoding=json&listing_type=buy&action=search_listings&page=1&place_name=london
 
     return 'http://api.nestoria.co.uk/api?' + querystring;
 };
